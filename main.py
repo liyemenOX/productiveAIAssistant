450bed94724a21953106c366116ac7b1c9d89f93
liyemenOX <kushagratripathi491@gmail.com>
2026-05-12 15:20:49 +0530

    added a feature to calculate the length of user input

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("Hello user, i am your assistant, how can i help you?")
 
 user = input("enter basic details about yourself:\n")
 print("thankyou for sharing your deatils here \n " + user + "\n i will use this information to assist you better")
+
+length = len(user)
+if(length > 10):
+    print("hey user your name is too lengthy")
+else:
+    print("hey! user your name is of satisfactory length")
