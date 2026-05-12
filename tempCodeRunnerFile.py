d6b691a6f94bfdfa27e4145465ec57a026068c3f
liyemenOX <kushagratripathi491@gmail.com>
2026-05-12 16:33:22 +0530

    added one more conditional for better interaction

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,12 @@
 print("Hello user, i am your assistant, how can i help you?")
 
 user = input("enter basic details about yourself:\n")
-print("thankyou for sharing your deatils here \n " + user + "\n i will use this information to assist you better")
+print("thankyou for sharing your deatils here \n " + user + "\n i will use this information to assist you better\n")
 
 length = len(user)
 if(length > 10):
-    print("hey user your name is too lengthy")
+    print("hey user your name is too lengthy\n")
+elif(length < 5):
+    print("huh !! your name is too short buddy\n")
 else:
-    print("hey! user your name is of satisfactory length")
+    print("hey! user your name is of satisfactory length\n")
