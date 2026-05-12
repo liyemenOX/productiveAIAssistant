36db5a6903957869d70e5270f9041acb9f798019
liyemenOX <kushagratripathi491@gmail.com>
2026-05-12 19:52:26 +0530

    added function named calclengthsuserinput to count for user input each time the function is called

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,10 +3,16 @@ print("Hello user, i am your assistant, how can i help you?")
 user = input("enter basic details about yourself:\n")
 print("thankyou for sharing your deatils here \n " + user + "\n i will use this information to assist you better\n")
 
-length = len(user)
-if(length > 10):
-    print("hey user your name is too lengthy\n")
-elif(length < 5):
-    print("huh !! your name is too short buddy\n")
-else:
-    print("hey! user your name is of satisfactory length\n")
+
+def calclengthsuserinput():
+        length = len(user)
+        if(length > 10):
+            print("hey user your name is too lengthy\n")
+        elif(length < 5):
+            print("huh !! your name is too short buddy\n")
+        else:
+            print("hey! user your name is of satisfactory length\n")
+    
+
+
+calclengthsuserinput()
